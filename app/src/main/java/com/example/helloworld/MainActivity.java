5dc163e7e175931cf8daae8f3d7cadea12eefcf0
Chanakan Mungtin <chanakan5591@chanakancloud.live>
2021-08-20 08:00:56 +0700

    Removed unnecessary methods, variables and class

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -18,36 +18,10 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
     private final TextView textView = findViewById(R.id.textView3);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //UpdateTime thread = new UpdateTime();
-        //thread.start();
-
-    }
-
-    public void sendMsg(View view){
-    Intent intent = new Intent(this, dispayMsg.class);
-    EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-    String message = editText.getText().toString();
-    intent.putExtra(EXTRA_MESSAGE, message);
-    startActivity(intent);
-
-    }
-
-    public void test(View view){
-        Intent intent = new Intent(this, test.class);
-        startActivity(intent);
-    }
-
-    public void date(){
-        Date date = new Date();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
-        String strDate = dateFormat.format(date);
-        textView.setText(strDate);
     }
 }
