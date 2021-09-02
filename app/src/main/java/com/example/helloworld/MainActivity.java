c1d12429022886868789035f293ff5b196ea5114
JourdanFK <jourd@chanakancloud.net>
2021-09-02 20:34:25 +0700

    Fixed App crashing during startup but still crashes when the Next button is used

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -9,24 +9,30 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final EditText editText = findViewById(R.id.editTextTextPersonName);
-    private final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("onCreate");
         super.onCreate(savedInstanceState);
+        System.out.println("Super");
         setContentView(R.layout.activity_main);
+        System.out.println("setContentView");
     }
     public void sendMsg(View view){
+        System.out.println("void sendMsg");
         Intent intent = new Intent(this, displayMsg.class);
+        System.out.println("new Intent()");
         EditText editText = findViewById(R.id.editTextTextPersonName);
+        System.out.println("findViewByID editText");
         String message = editText.getText().toString();
+        System.out.println("String Message");
+        String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
+        System.out.println("String EXTRA_MESSAGE");
         intent.putExtra(EXTRA_MESSAGE, message);
+        System.out.println("Intent putExtra");
         startActivity(intent);
+        System.out.println(intent);
+        System.out.println("startActivity");
 
-    }
-    public void blankMsg(View view){
-        String blank = "";
-        editText.setText(blank);
     }
 }
--- app/src/main/java/com/example/helloworld/displayMsg.java
+++ app/src/main/java/com/example/helloworld/displayMsg.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,31 +10,31 @@ import android.widget.TextView;
 
 
 public class displayMsg extends AppCompatActivity {
-    private final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
+
     private final WebView myWebView = findViewById(R.id.webview);
     private final TextView textView = findViewById(R.id.textView);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_msg);
 
+        String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
         Intent intent = getIntent();
-        String message = intent.getStringExtra(this.EXTRA_MESSAGE);
-
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        System.out.println(message);
         textView.setText(message);
 
         switch(message.toLowerCase()) {
             case "chanakan": {
-                textView.setText("ello chanakan, this is an if statement");
+                textView.setText("Get redirected to your own website");
                 WebViewChanakan();
             } break;
             case "sudo test": {
                 test();
             } break;
             default: {
-                textView.setText("ERROR, You haven't entered anything yet");
+                textView.setText("Enter something and try again");
             } break;
         }
 
